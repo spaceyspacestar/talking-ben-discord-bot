292ec69fdf6652dcf3db6b6e3ef9ce903e93c156
Space Fujisaki <41171313+spaceyspacestar@users.noreply.github.com>
2022-03-02 22:29:17 -0800

    Add "new" command: hangup
    This command pretty much sets the variable integer to 14.
    
    Oh and also fix stuff as this was rushed imo

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {Client, Intents, User, Interaction, MessageReaction} = require('discord.js');
+const {Client, Intents} = require('discord.js');
 const {token} = require("./config.json");
 
 //Init Client
@@ -17,6 +17,7 @@ let incall = false;
 let tilhangup = 0;
 let channelID = null; //Store channel info to send hang up message 
 let PhoneID;
+let callstate = 0; //avoid breaking with the callben command
 
 client.on('messageCreate', async message => {
     if (message.author.bot) return;
@@ -36,7 +37,7 @@ client.on('messageCreate', async message => {
             else {
                 let response = ['Uuuggghhhh...', 'Ho ho ho..!', 'No.', 'Yeeesss.', '...'];
                 let answer = (Math.floor(Math.random() * Math.floor(response.length)));
-                if (answer === 4){ //Aka the "..." reponse, don't reset timer
+                if (answer === 4){ //Aka the "..." reponse, don't reset the counter
                     message.channel.send({content: response[answer]});
                 }  
                 else {
@@ -48,25 +49,59 @@ client.on('messageCreate', async message => {
     }
     else if (command === 'callben')
     {
-        msg = await message.channel.send('☎️ *ring ring*');
-        channelID = message.channel.id;
-        setTimeout(() => {PickUpThePhoneBaby()}, 3000);
+        switch(callstate)
+        {
+            case 0: //Not in a phone right now
+                msg = await message.channel.send('☎️ *ring ring*');
+                channelID = message.channel.id;
+                guildname = message.guild.name; //so we can print the server name
+                setTimeout(() => {PickUpThePhoneBaby()}, 3000);
+                callstate = 1;
+                break;
+            case 1: //Currently in a phone
+                if (message.member.channelID === message.guild.channelID){
+                    message.channel.send(`You are already on the phone with Ben...\nGo to <#${channelID} so you can ask him questions`);
+                }
+                break;
+        }
+
+    }
+    else if (command === 'hangup')
+    {
+        switch(callstate)
+        {
+            case 0:
+                message.channel.send("You are not in a call currently.")
+                return;
+            case 1:
+                tilhangup = 14; //lol
+                break;
+        }
     }
 });
 
+//Time
+var currentdate = new Date(); 
+var datetime =  + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
 
 //Telephone bullshit
 async function StartCall() {
     if (!PhoneID) {
         PhoneID = setInterval(function(){
             tilhangup++;
-            console.log(tilhangup);
+            console.log(`Hang up integer: ${tilhangup}`)
             //Now Hang up until it reaches these seconds
             if (tilhangup > 14) {
                 if (tilhangup == 15)
                 {
                     try{
                         client.channels.cache.get(channelID).send("*hangs up phone*");
+                        console.log("Hunged up the phone");
                         channelID = null;
                     }
                     catch(e){
@@ -75,6 +110,7 @@ async function StartCall() {
                 }
                 incall = false;
                 tilhangup = 0;
+                callstate = 0;
                 clearInterval(PhoneID);
                 PhoneID = null;
                 client.user.setPresence({ activities:[{name: 'users talk || b!ask', type:'LISTENING'}], status:'online'} );
@@ -86,8 +122,8 @@ async function StartCall() {
 async function PickUpThePhoneBaby() {
     msg.edit('*picks up phone*');
     incall = true;
+    console.log(`Initiated a call in ${guildname} at ${datetime}`);
     client.user.setPresence({ activities:[{name: 'your call || b!ask', type:'LISTENING'}], status:'dnd'} );
-    console.log(channelID);
     client.channels.cache.get(channelID).send("Ben?");
     StartCall();
 }
